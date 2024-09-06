81170a5952fcc46a2d9bb1102ab1d262e4adfc1e
Boumety <162808181+Boumety@users.noreply.github.com>
2024-09-06 21:50:12 +0200

    Create groups.lua

---
+++ groups/groups.lua
@@ -0,0 +1,28 @@
+local mod = {}
+local groups = {}
+
+function mod.addToGroup(self, object, name)
+   if not groups[name] then
+      groups[name] = {}
+   end
+
+   local group = groups[name]
+   table.insert(group, object)
+end
+
+function mod.callGroup(self, name, funcName)
+   local group = groups[name]
+
+   if not group then
+      error("'" .. name .. "'" .. " is not a valid group")
+   end
+
+   for _, obj in pairs(group) do
+      local func = obj[funcName]
+      if func then 
+         func()
+      end
+   end
+end
+
+return mod
